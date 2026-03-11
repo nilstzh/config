db532d625fa80f28bc08e81bb1e504481b367ba2
Nils Truzhenko <nils.tzh@gmail.com>
2026-03-11 14:05:38 +0200

    fish: add theme setup script

---
+++ .config/fish/conf.d/setup.fish
@@ -0,0 +1,30 @@
+set -U fish_color_command 91BA9C
+
+set -U tide_left_prompt_items time pwd git newline character
+set -U tide_right_prompt_items
+
+set -U tide_time_bg_color 8884A5
+set -U tide_time_color 272E39
+set -U tide_time_format '%H:%M'
+
+set -U tide_pwd_bg_color 7DA0A7
+set -U tide_pwd_color_anchors 272E39
+set -U tide_pwd_color_dirs 272E39
+set -U tide_pwd_color_truncated_dirs 272E39
+
+set -U tide_git_bg_color 91BA9C
+set -U tide_git_color_branch 272E39
+set -U tide_git_color_stash 272E39
+set -U tide_git_color_operation 272E39
+set -U tide_git_color_upstream 272E39
+set -U tide_git_color_conflicted 272E39
+set -U tide_git_color_staged 272E39
+set -U tide_git_color_dirty 272E39
+set -U tide_git_color_untracked 272E39
+set -U tide_git_truncation_length 30
+
+set -U tide_character_icon ❯
+set -U tide_character_color 91BA9C
+set -U tide_character_color_failure CC92B9
+
+set -U tide_prompt_color_frame_and_connection 2F3D53
